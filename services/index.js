34b9db7456ec21f0505832b7c535406ad2bf3172
Shmidt <volodymyrshmidt@gmail.com>
2021-11-22 23:32:01 +0100

    add post widget

--- services/index.js
+++ services/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { request, gql } from "graphql-request";
-const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
 export const getPosts = async () => {
   const query = gql`
     query MyQuery {
@@ -32,6 +34,47 @@ export const getPosts = async () => {
     }
   `;
 
-  const result = await request(graphqlAPI, query)
-  return result.postsConnection.edges
+  const result = await request(graphqlAPI, query);
+  return result.postsConnection.edges;
+};
+
+export const getRecentPosts = async () => {
+  const query = gql`
+  query GetPostDetails(){
+    posts(
+      orderBy: createdAt_ASC
+      last: 3
+    ){
+      title
+      featuredImage{
+        url
+      }
+      createdAt
+      slug
+    }
+  }
+  `;
+
+  const result = await request(graphqlAPI, query);
+  return result.posts;
+};
+
+export const getSimilarPosts = async () => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]){
+posts(
+  where: {slug_not: $slug, AND {categories_some: {slug_in: $categories}}}
+  last: 3
+){
+  title
+  featuredImage{
+    url
+  }
+  createdAt
+  slug
+}
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+  return result.posts;
 };
